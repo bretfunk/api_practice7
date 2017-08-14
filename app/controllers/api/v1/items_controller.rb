e31ab59555e9afe5510a57defdd3b3a06ea08643
BFunk <bret.e.funk@gmail.com>
2017-08-14 14:07:48 -0600

    added the rest of CRUD

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::ItemsController < ApplicationController
+  def index
+    render json: Item.all
+  end
+
+  def show
+    render json: Item.find(params[:id])
+  end
+
+  def create
+    render json: Item.create(item_params)
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    render json: @item.update(item_params)
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    render json: @item.destroy
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description)
+  end
+end
--- spec/requests/item_api_spec.rb
+++ spec/requests/item_api_spec.rb
@@ -9,4 +9,41 @@ RSpec.describe "APIs" do
     items = JSON.parse(response.body)
     expect(items.count).to eq(3)
   end
+  it "can get one item by its id" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+    expect(response).to be_success
+  end
+  it "can create a new item" do
+    item_params = {name: "Saw",
+                   description: "I want to play a game"}
+    post "/api/v1/items", params: {item: item_params}
+    expect(response).to be_success
+    item = Item.last
+
+    expect(item.name).to eq(item_params[:name])
+    expect(item.description).to eq(item_params[:description])
+  end
+
+  it "can update an existing item" do
+    id = create(:item).id
+    previous_name = Item.last.name
+    item_params = {name: "Saw",
+                   description: "I want to play a game"}
+    put "/api/v1/items/#{id}", params: {item: item_params}
+    item = Item.find_by(id: id)
+    expect(response).to be_success
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq(item_params[:name])
+  end
+  it "can delete an item" do
+    item = create(:item)
+
+    expect(Item.count).to eq(1)
+    delete "/api/v1/items/#{item.id}"
+    expect(response).to be_success
+    expect(Item.count).to eq(0)
+    expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
